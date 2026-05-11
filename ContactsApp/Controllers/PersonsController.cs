1a3ee18ac22b5e4186d5e23cc239f522f43f70cd
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2026-05-11 18:06:18 +0800

    feat:added UI for persons list page

--- ContactsApp/Controllers/PersonsController.cs
+++ ContactsApp/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ServiceContracts;
+using ServiceContracts.DTO;
 
 namespace ContactsApp.Controllers;
 
@@ -8,12 +9,10 @@ public class PersonsController : Controller
 
     private readonly IPersonService _personService;
 
-
     public PersonsController(IPersonService personService)
     {
         _personService = personService;
     }
-    
     
     // GET
     [HttpGet]
@@ -21,6 +20,7 @@ public class PersonsController : Controller
     [Route("/")]
     public IActionResult Index()
     {
-        return View();
+        List<PersonResponse> persons = _personService.GetAllPersons();
+        return View(persons);
     }
 }
--- ContactsApp/Program.cs
+++ ContactsApp/Program.cs
@@ -4,13 +4,17 @@ using Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
+// Add services to IOC container
 builder.Services.AddScoped<ICountriesService,CountriesService>();
 builder.Services.AddScoped<IPersonService,PersonService>();
 
 
 var app = builder.Build();
 
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 
 app.UseStaticFiles();
